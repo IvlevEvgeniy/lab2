47950480fa5b9d8ed867b698ddd3fb4306b18f78
IvlevEvgeniy <79964221+IvlevEvgeniy@users.noreply.github.com>
2021-03-28 08:35:00 +0300

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include <math.h>
+#define n 3
+using namespace std;
+int main()
+{
+    int i;
+    double x, sum = 0, add = 1;
+    do {
+                  
+            cout << "Input |x| < 1: " << endl;           
+            cin >> x; 
+              
+        } while (fabs(x) >= 1);
+     
+     
+    for (i = 0; i <= n; ++i, add = ((i + 1) * pow(x,i)))
+        sum += add;
+
+    cout << "Sum=" <<sum << endl;
+    return 0;
+}
