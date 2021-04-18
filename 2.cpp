bd5ca512876f0ff353a4246d05842ad38b47f9c9
IvlevEvgeniy <79964221+IvlevEvgeniy@users.noreply.github.com>
2021-04-18 11:00:08 +0300

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -1,21 +1,32 @@
-
 #include <iostream>
 #include <stdio.h>
 #include <math.h>
 #define n 3
 using namespace std;
+double getValue()
+{
+    while (true) 
+    {
+        cout << "Input |x| < 1: ";
+        double x;
+        cin >> x;
+
+        if (cin.fail() or (fabs(x) >= 1))
+        {
+            cin.clear(); 
+            cin.ignore(32767, '\n'); 
+        }
+        else
+            return x;
+    }
+}
+
 int main()
 {
     int i;
     double x, sum = 0, add = 1;
     
-    do {
-            do{
-                cout << "Input |x| < 1: " << endl;
-                cin >> x;
-                cin.clear();
-            } while (cin.get() != '\n');        
-    } while (fabs(x) >= 1);
+   x = getValue();
 
     for (i = 0; i <= n; ++i, add = ((i + 1) * pow(x, i)))
         sum += add;
